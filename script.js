bbdf7df1ef47b7e4aa94764ef0234ed749c0c996
Beau Sherman <brsherman88@gmail.com>
2020-08-25 12:06:49 -0700

    First commit

---
+++ script.js
@@ -0,0 +1,25 @@
+$(document).ready(() => {
+  $('.main__carousel').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 670,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  })
+})
